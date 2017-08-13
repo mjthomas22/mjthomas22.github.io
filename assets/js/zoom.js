6fdc2a42575456b8ab5605a9acb78eb9f637fc88
mjthomas22 <mthomas22@gmail.com>
2017-08-13 11:56:33 -0400

    finished adding jquery

--- assets/js/zoom.js
+++ assets/js/zoom.js
@@ -1,10 +1,36 @@
 ﻿$(document).ready(function () {
-    var bankText = $('<p>Used C# to create parent and child classes, to inherit properties from the parent class allowing them to interact with each other.Users can withdraw and deposit funds and check account balances. Deposit account has a feature making users unable to over withdraw from this account.</p >');
-               
-    $(".image").click(function () {
-        $(this).toggleClass("imgClick");
-        $(".projectList, .projectText, .major, .special").toggleClass("hide");
+    var bankImg = "<img id=\"bigBankPicture\" class=\"bigImg img-responsive\" src=\"images/bankAccount.png\" />";
+    var reviewImg = "<img id=\"bigReviewPicture\" class=\"bigImg img-responsive\" src=\"images/Reviews.png\" />"; 
+    var wardrobeImg = "<img id=\"bigWardrobePicture\" class=\"bigImg img-responsive\" src=\"images/edmx.png\" />";
+
+    $("#bankPicture").click(function () {
+        $("#one").toggleClass("hide");
+        $("#two").before(bankImg);
+
+        $("#bigBankPicture").click(function () {
+            $("#one").toggleClass("hide");
+            $("#bigBankPicture").remove();
+        });
     });
 
+    $("#reviewPicture").click(function () {
+        $("#two").toggleClass("hide");
+        $("#three").before(reviewImg);
+
+        $("#bigReviewPicture").click(function () {
+            $("#two").toggleClass("hide");
+            $("#bigReviewPicture").remove();
+        });
+    });
+
+    $("#wardrobePicture").click(function () {
+        $("#three").toggleClass("hide");
+        $("#four").before(wardrobeImg);
+
+        $("#bigWardrobePicture").click(function () {
+            $("#three").toggleClass("hide");
+            $("#bigWardrobePicture").remove();
+        });
+    });
 
 });
