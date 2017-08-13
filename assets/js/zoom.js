78cf347c45fed6f4d451ae88dd2f607d6876eb5b
mjthomas22 <mthomas22@gmail.com>
2017-08-13 10:12:24 -0400

    working on pic zoom

--- assets/js/zoom.js
+++ assets/js/zoom.js
@@ -1,5 +1,10 @@
 ﻿$(document).ready(function () {
+    var bankText = $('<p>Used C# to create parent and child classes, to inherit properties from the parent class allowing them to interact with each other.Users can withdraw and deposit funds and check account balances. Deposit account has a feature making users unable to over withdraw from this account.</p >');
+               
     $(".image").click(function () {
         $(this).toggleClass("imgClick");
+        $(".projectList, .projectText, .major, .special").toggleClass("hide");
     });
+
+
 });
